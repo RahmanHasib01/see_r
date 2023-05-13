58fcd2299fbf5b7fa51d4251893f9ec3049ac768
Rahmanhasib01 <rahmanhasib.rezaur@gmail.com>
2023-05-13 21:30:16 +0800

    add pages/home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  return runApp(MaterialApp(
+    title: 'My App',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  ));
+}
--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
 # The notes for flutter
 # 2023-05-13
-
+```
+flutter create <project name> // Create a new flutter project
+```
