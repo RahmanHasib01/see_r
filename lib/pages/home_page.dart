04ce171c2d796d12a88890d1f66d547bf5fd71f4
Rahmanhasib01 <rahmanhasib.rezaur@gmail.com>
2023-05-14 20:09:48 +0800

    NAV BAR

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,24 +7,33 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        bottomNavigationBar: const GNav(
-          backgroundColor: Color.fromARGB(255, 240, 234, 210),
-          color: Color.fromARGB(255, 100, 56, 1),
-          gap: 5,
-          tabs: [
-            GButton(icon: Icons.home, text: 'Home'),
-            GButton(icon: Icons.notifications_rounded, text: 'Notifications'),
-            GButton(icon: Icons.play_circle, text: 'Playback'),
-            GButton(icon: Icons.more, text: 'More'),
-          ],
+        bottomNavigationBar: Container(
+          color: const Color.fromARGB(255, 240, 234, 210),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+            child: GNav(
+              backgroundColor: Color.fromARGB(255, 240, 234, 210),
+              color: Color.fromARGB(255, 100, 56, 1),
+              tabBackgroundColor: Color.fromARGB(130, 100, 55, 1),
+              padding: EdgeInsets.all(15),
+              tabs: [
+                GButton(icon: Icons.home, text: 'Home'),
+                GButton(
+                    icon: Icons.notifications_rounded, text: 'Notifications'),
+                GButton(icon: Icons.play_circle, text: 'Playback'),
+                GButton(icon: Icons.more, text: 'More'),
+              ],
+            ),
+          ),
         ),
         body: Container(
             width: double.infinity,
             height: double.infinity,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/images/background.jpg'),
+                image: AssetImage('assets/images/Search.jpg'),
                 fit: BoxFit.cover,
               ),
             )),
