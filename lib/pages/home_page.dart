e3d4a6f475ba7458fcd3a6c8e2c93b4a9a76c8aa
Rahmanhasib01 <rahmanhasib.rezaur@gmail.com>
2023-05-14 17:49:04 +0800

    home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,16 +7,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/Search.jpg'),
-              fit: BoxFit.cover,
-            ),
-          )),
-    ));
+      home: Scaffold(
+        bottomNavigationBar: const GNav(
+          backgroundColor: Color.fromARGB(255, 240, 234, 210),
+          color: Color.fromARGB(255, 100, 56, 1),
+          gap: 5,
+          tabs: [
+            GButton(icon: Icons.home, text: 'Home'),
+            GButton(icon: Icons.notifications_rounded, text: 'Notifications'),
+            GButton(icon: Icons.play_circle, text: 'Playback'),
+            GButton(icon: Icons.more, text: 'More'),
+          ],
+        ),
+        body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            )),
+      ),
+    );
   }
 }
