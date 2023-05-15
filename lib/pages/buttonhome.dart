ce08e979b7a91adb18d6c69d46b70f299efe4941
Rahmanhasib01 <rahmanhasib.rezaur@gmail.com>
2023-05-15 11:37:25 +0800

    button

---
+++ lib/pages/buttonhome.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final ButtonStyle buttonhome = ElevatedButton.styleFrom(
+  backgroundColor: const Color.fromARGB(100, 100, 79, 56),
+  minimumSize: const Size(60, 60),
+  padding: const EdgeInsets.symmetric(horizontal: 16),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(200)),
+  ),
+);
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'buttonhome.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,34 +10,51 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+// bottom Navigation Bar
         bottomNavigationBar: Container(
           color: const Color.fromARGB(255, 240, 234, 210),
           child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 7),
             child: GNav(
               backgroundColor: Color.fromARGB(255, 240, 234, 210),
               color: Color.fromARGB(255, 100, 56, 1),
-              tabBackgroundColor: Color.fromARGB(130, 100, 55, 1),
-              padding: EdgeInsets.all(15),
+              tabBackgroundColor: Color.fromARGB(35, 100, 55, 1),
+              padding: EdgeInsets.all(10),
               tabs: [
                 GButton(icon: Icons.home, text: 'Home'),
                 GButton(
                     icon: Icons.notifications_rounded, text: 'Notifications'),
                 GButton(icon: Icons.play_circle, text: 'Playback'),
-                GButton(icon: Icons.more, text: 'More'),
+                GButton(icon: Icons.more, text: ' More'),
               ],
             ),
           ),
         ),
-        body: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/Search.jpg'),
-                fit: BoxFit.cover,
+// Bottom Navigation Bar
+
+        body: Stack(
+          children: [
+            Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Search.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                )),
+            Center(
+              child: ElevatedButton(
+                style: buttonhome,
+                onPressed: () {
+                  // Add your button click logic here.
+                },
+                child: const Text('Button Text'),
               ),
-            )),
+            ),
+          ],
+        ),
+// body
       ),
     );
   }
