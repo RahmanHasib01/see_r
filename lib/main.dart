7e276cb9660c217a8d8a5ed09c4d4086e63fc3ae
Rahmanhasib01 <rahmanhasib.rezaur@gmail.com>
2023-05-24 18:22:10 +0800

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:see_r/splashscreen.dart';
 import 'firebase_options.dart';
 import 'package:web_socket_channel/io.dart';
 
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const SplashScreen(),
       routes: {
         '/home': (context) => const HomePage(),
         '/notifications': (context) => const NotificationsPage(),
